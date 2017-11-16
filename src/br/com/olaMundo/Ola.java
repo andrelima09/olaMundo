2ba8fb0a1c26c5447b28de151b91cc891cfc9ce2
root <andrefl09@gmail.com>
2017-11-16 15:05:50 -0200

    teste

---
+++ src/br/com/olaMundo/Ola.java
@@ -0,0 +1,16 @@
+package br.com.olaMundo;
+
+public class Ola {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println(oi());
+	}
+	
+	public static String oi() {
+		String oi = "Oi mundo";
+		return oi;
+	}
+	
+}
